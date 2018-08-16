c9fc08c9b8e285cc41d3db17f4e40f83b1c2c78d
Grigor Zotov <grigorzotov@gmail.com>
2018-08-16 20:33:59 +0300

    Add files via upload
    Player movement script and collision.

---
+++ PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+
+    void OnCollisionEnter(Collision colliderInfo) {
+        if (colliderInfo.collider.tag == "Obstacle") {
+            GetComponent<PlayerMovement>().enabled = false;
+            FindObjectOfType<GameManagerSrc>().GameOver();
+        }
+    }
+
+
+}
